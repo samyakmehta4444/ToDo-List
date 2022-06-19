a631cbfe92b9b6e128fd22c752aeb656abbd31b0
Samyak Mehta <83632041+samyakmehta4444@users.noreply.github.com>
2022-06-19 16:30:05 +0530

    UPDATE app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-samyak:Test123@cluster0.lk41j.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-samyak:<PASSWORD>@cluster0.lk41j.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
